f9bd4b16a32a13c5a645878176a27a55e86a6fb3
Alf <iamamccabe@gmail.com>
2025-11-14 09:40:05 +0000

    Change package execution method to use exec
    Replace os.system call with exec to run the package script.

--- packages/com.mralfiem591.paxd/src/run_pkg.py
+++ packages/com.mralfiem591.paxd/src/run_pkg.py
@@ -73,4 +73,5 @@ sys.meta_path.insert(0, PaxDSDKFinder())
 
 # Step 2: run the package, with adjusted sys.argv (remove the first argument, which is the run_pkg.py path, to prevent issues with modules like argparse thinking the main script is named run_pkg.py and not the actual package script)
 sys.argv = sys.argv[1:]
-os.system(f'uv run {" ".join(sys.argv)}')
+with open(sys.argv[1], "r", encoding="utf-8") as f:
+    exec(f.read())
