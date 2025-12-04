31e0725f3df1894477e5fab15a85c68d8c025a35
mralfiem591 <iamamccabe@gmail.com>
2025-12-04 11:32:29 +0000

    fix: paxd-gui wont start with error "Error importing PaxD GUI: No module named 'paxd_gui'"

--- packages/com.mralfiem591.paxd-gui/src/launch.py
+++ packages/com.mralfiem591.paxd-gui/src/launch.py
@@ -9,8 +9,7 @@ import os
 
 # Add the src directory to the Python path
 current_dir = os.path.dirname(os.path.abspath(__file__))
-src_dir = os.path.join(current_dir, 'src')
-sys.path.insert(0, src_dir)
+sys.path.insert(0, current_dir)
 
 try:
     from paxd_gui import PaxDGUI
